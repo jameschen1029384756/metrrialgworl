396b66ef55533e991405a5eb4ece6567fbe0fcfc
James Chen <83474000+jameschen1029384756@users.noreply.github.com>
2022-05-22 15:50:16 -0700

    Update supportedChainId.ts

--- src/utils/supportedChainId.ts
+++ src/utils/supportedChainId.ts
@@ -1,11 +1,11 @@
-import { SupportedChainId } from '../constants/chains'
+import { SupportedChainId } from '../connectors/index'
 
 /**
  * Returns the input chain ID if chain is supported. If not, return undefined
  * @param chainId a chain ID, which will be returned if it is a supported chain ID
  */
-export function supportedChainId(chainId: number | undefined): SupportedChainId | undefined {
-  if (typeof chainId === 'number' && chainId in SupportedChainId) {
+export function supportedChainIds(chainId: number | undefined): SupportedChainIds | undefined {
+  if (typeof chainId === 'number' && chainId in SupportedChainIds) {
     return chainId
   }
   return undefined
