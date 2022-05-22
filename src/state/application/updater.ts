832ca24c64abd1b45dd37557f820a2e93207fd67
James Chen <83474000+jameschen1029384756@users.noreply.github.com>
2022-05-22 12:51:08 -0700

    Update updater.ts

--- src/state/application/updater.ts
+++ src/state/application/updater.ts
@@ -1,57 +1,47 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useActiveWeb3React } from '../../hooks';
-import useDebounce from '../../hooks/useDebounce';
-import useIsWindowVisible from '../../hooks/useIsWindowVisible';
-import { updateBlockNumber } from './actions';
-import { useDispatch } from 'react-redux';
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
+import useDebounce from 'hooks/useDebounce'
+import useIsWindowVisible from 'hooks/useIsWindowVisible'
+import { useEffect, useState } from 'react'
+import { api, CHAIN_TAG } from 'state/data/enhanced'
+import { useAppDispatch, useAppSelector } from 'state/hooks'
+import { supportedChainId } from 'utils/supportedChainId'
+
+import { updateChainId } from './reducer'
+
+function useQueryCacheInvalidator() {
+  const dispatch = useAppDispatch()
+
+  // subscribe to `chainId` changes in the redux store rather than Web3
+  // this will ensure that when `invalidateTags` is called, the latest
+  // `chainId` is available in redux to build the subgraph url
+  const chainId = useAppSelector((state) => state.application.chainId)
+
+  useEffect(() => {
+    dispatch(api.util.invalidateTags([CHAIN_TAG]))
+  }, [chainId, dispatch])
+}
 
 export default function Updater(): null {
-  const { library, chainId } = useActiveWeb3React();
-  const dispatch = useDispatch();
+  const { chainId, library } = useActiveWeb3React()
+  const dispatch = useAppDispatch()
+  const windowVisible = useIsWindowVisible()
 
-  const windowVisible = useIsWindowVisible();
+  const [activeChainId, setActiveChainId] = useState(chainId)
 
-  const [state, setState] = useState<{ chainId: number | undefined; blockNumber: number | null }>({
-    chainId,
-    blockNumber: null,
-  });
-
-  const blockNumberCallback = useCallback(
-    (blockNumber: number) => {
-      setState((state) => {
-        if (chainId === state.chainId) {
-          if (typeof state.blockNumber !== 'number') return { chainId, blockNumber };
-          return { chainId, blockNumber: Math.max(blockNumber, state.blockNumber) };
-        }
-        return state;
-      });
-    },
-    [chainId, setState]
-  );
-
-  // attach/detach listeners
-  useEffect(() => {
-    if (!library || !chainId || !windowVisible) return undefined;
-
-    setState({ chainId, blockNumber: null });
-
-    library
-      .getBlockNumber()
-      .then(blockNumberCallback)
-      .catch((error) => console.error(`Failed to get block number for chainId: ${chainId}`, error));
-
-    library.on('block', blockNumberCallback);
-    return () => {
-      library.removeListener('block', blockNumberCallback);
-    };
-  }, [dispatch, chainId, library, blockNumberCallback, windowVisible]);
-
-  const debouncedState = useDebounce(state, 100);
+  useQueryCacheInvalidator()
 
   useEffect(() => {
-    if (!debouncedState.chainId || !debouncedState.blockNumber || !windowVisible) return;
-    dispatch(updateBlockNumber({ chainId: debouncedState.chainId, blockNumber: debouncedState.blockNumber }));
-  }, [windowVisible, dispatch, debouncedState.blockNumber, debouncedState.chainId]);
+    if (library && chainId && windowVisible) {
+      setActiveChainId(chainId)
+    }
+  }, [dispatch, chainId, library, windowVisible])
 
-  return null;
+  const debouncedChainId = useDebounce(activeChainId, 100)
+
+  useEffect(() => {
+    const chainId = debouncedChainId ? supportedChainId(debouncedChainId) ?? null : null
+    dispatch(updateChainId({ chainId }))
+  }, [dispatch, debouncedChainId])
+
+  return null
 }
